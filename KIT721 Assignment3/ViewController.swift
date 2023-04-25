dd8a7cd64516f669d7ccd3e0df8a4ba7ef6ff681
daviduang <384755564@qq.com>
2023-04-25 22:02:32 +1000

    Initial Commit

---
+++ KIT721 Assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KIT721 Assignment3
+//
+//  Created by Yi Wang on 25/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
